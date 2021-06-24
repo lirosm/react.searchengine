e364094ab52fe1f2e9288397a83b9b77a0e7ecd3
Liridona <liridonaosm@gmail.com>
2021-06-24 16:29:49 +0200

    added Github URL

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import react from "react";
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return <div className="App">
+    <h1>Weather app</h1>
+
+    <footer>
+      This project is coded by Liridona Osmani {""}
+    <a href="https://github.com/lirosm/react.searchengine" target="_blank">and is open-sourced on GitHub
+    </a> 
+   </footer> 
+   
+   </div>;
 }
 
 
